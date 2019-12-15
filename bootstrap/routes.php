c15ee82c30edbc309df1233b8414e01890096a43
Tatyana Vogel <tatyana.vogel@gibmit.ch>
2019-12-15 15:10:53 +0100

    Replace Slim with Siler

---
+++ bootstrap/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+use function Siler\Functional\puts;
+use function Siler\Route\get;
+
+get('/', puts('chello'));
--- bootstrap/settings.php
+++ bootstrap/settings.php
@@ -1,19 +1,10 @@
 <?php
-declare(strict_types=1);
-
-use DI\ContainerBuilder;
-
-return function (ContainerBuilder $containerBuilder) {
     // Global Settings Object
-    $containerBuilder->addDefinitions([
-        'settings' => [
+return [
             'db' => [
                 'host' => isset($_ENV['docker']) ? 'db' : '127.0.0.1',
                 'name' => getenv('DB_NAME'),
                 'user' => getenv('DB_USER'),
                 'pwd' => getenv('DB_PASSWORD'),
             ],
-            'displayErrorDetails' => true, // Should be set to false in production
-        ],
-    ]);
-};
+    ];

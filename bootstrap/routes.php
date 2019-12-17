15f08a45104831f97458c36e61ff8c76dbd3a9dd
Tatyana Vogel <tatyana.vogel@gibmit.ch>
2019-12-17 15:20:21 +0100

    Forgot to save merge

--- bootstrap/routes.php
+++ bootstrap/routes.php
@@ -7,20 +7,17 @@ use function Siler\Route\resource;
 
 get('/', puts('chello'));
 
-<<<<<<< HEAD
 get('/login', '../endpoints/login.php');
 post('/login', '../endpoints/auth_user.php');
 post('/logout', '../endpoints/logout.php');
 get('/register', '../endpoints/register.php');
 
 
-=======
 /**
  * Registers routes for all seven CRUD actions, on the users resource.
  * Each action is represented by a php file with the action name.
  * Index, Create, Show, Store, Edit, Update, Delete.
  */
->>>>>>> master
 resource('/users', '../endpoints/users');
 
 /**

cd771c7fe9a6ab0e779b886664264ed4a942de59
hactar <hactar@cyberkraft.ch>
2019-12-17 15:16:25 +0100

    Fix twig env

--- bootstrap/setup.php
+++ bootstrap/setup.php
@@ -7,7 +7,7 @@ use function Siler\Http\flash;
 $dotenv = Dotenv\Dotenv::create(__DIR__.'/..');
 $dotenv->load();
 
-Twig\init('../resources/templates/')
-    ->addGlobal('errorAlert', flash('errorAlert'))
-    ->addGlobal('infoAlert', flash('infoAlert'));
+$twigEnv = Twig\init('../resources/templates/');
+$twigEnv->addGlobal('errorAlert', flash('errorAlert'));
+$twigEnv->addGlobal('infoAlert', flash('infoAlert'));
 

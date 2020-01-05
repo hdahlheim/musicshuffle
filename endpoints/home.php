219c65092c15a9678d331fdf52ea51912a4fdee0
hactar <hactar@cyberkraft.ch>
2020-01-05 16:56:14 +0100

    Pagination done

--- endpoints/home.php
+++ endpoints/home.php
@@ -4,13 +4,20 @@ use function Siler\Http\Response\html;
 use function Siler\Twig\render;
 use function Auth\isUserLoggedin;
 use function Database\getFivePlaylistsOfUser;
+use function Database\getTotalNumberPlaylistsOfUser;
+use function Siler\Http\Request\get;
 use function Siler\Http\session;
 
-
-$playlists = getFivePlaylistsOfUser(session('user_id'));
-
 if (isUserLoggedin()) {
-    html(render('home/home_loggedIn.twig', compact('playlists')));
+    $limit = 5;
+
+    $page = (int) get('p', 1);
+
+    $total = getTotalNumberPlaylistsOfUser(session('user_id'));
+
+    $playlists = getFivePlaylistsOfUser(session('user_id'), $page);
+
+    html(render('home/home_loggedIn.twig', compact('playlists', 'page', 'limit', 'total')));
 } else {
     html(render('home/home_loggedOut.twig'));
 }
